b3ca5baf3482c65a94279542e5b9937d59f4ae2f
Dienay Lima <dienaylima@gmail.com>
2024-05-19 16:14:07 -0300

    feature: create index config

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+import logger from './logger';
+import env from './env';
+import { app, routes, RequestProps, ResponseProps, NextFunctionProps } from './http';
+
+export { logger, env, app, routes, RequestProps, ResponseProps, NextFunctionProps };
