099c7656dad388ce95cba467a8020545ece0c6a4
Dienay Lima <dienaylima@gmail.com>
2024-05-19 16:45:19 -0300

    improvement: update db connect

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -1,21 +1,22 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import 'dotenv/config';
+import logger from './logger';
 
-const { URI } = process.env
+const { URI } = process.env;
 
-const uri: string = URI || "localhost";
+const uri: string = URI || 'localhost';
 
 async function run() {
   try {
     await mongoose.connect(uri);
 
-    console.log("You successfully connected to MongoDB!");
+    logger.info('You successfully connected to MongoDB!');
 
     return mongoose.connection;
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    logger.error('Error connecting to MongoDB:', error);
     throw error;
   }
 }
 
 export default run;
-
