80cb2c2bde4ff99c261163d99758b65ab0176c9e
Dienay Lima <dienaylima@gmail.com>
2024-05-19 16:31:11 -0300

    feature: Create bad request error

---
+++ src/errors/BadRequest.ts
@@ -0,0 +1,9 @@
+import BaseError from './BaseError';
+
+class BadRequest extends BaseError {
+  constructor(message = 'Bad Request, incorrect input data') {
+    super(message, 400);
+  }
+}
+
+export default BadRequest;
