dd61476b3e7a04cd3b77d88d87ee14351ee50bf3
Dienay Lima <dienaylima@gmail.com>
2024-05-19 16:30:40 -0300

    feature: Create base error

---
+++ src/errors/BaseError.ts
@@ -0,0 +1,19 @@
+import { Response } from 'express';
+
+class BaseError extends Error {
+  status: number;
+
+  constructor(message: string = 'Internal Error', status = 500) {
+    super(message);
+    this.status = status;
+  }
+
+  sendResponse(res: Response) {
+    res.status(this.status).json({
+      message: this.message,
+      status: this.status
+    });
+  }
+}
+
+export default BaseError;
