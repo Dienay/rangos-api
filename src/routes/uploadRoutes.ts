c4da0399c18b0e5034856ed8ec889c8d0b9e22cf
Dienay Lima <dienaylima@gmail.com>
2024-06-10 10:09:32 -0300

    improvement: update upload routes

--- src/routes/uploadRoutes.ts
+++ src/routes/uploadRoutes.ts
@@ -1,13 +1,24 @@
 import { Router } from 'express';
-import { establishmentsUpload } from '@/config/multer';
+import { establishmentsUpload, productsUpload, usersUpload } from '@/config/multer';
+import { Multer } from 'multer';
+import { RequestProps, ResponseProps } from '@/config';
 
 const uploadRouter = Router();
 
-uploadRouter.post('/establishments', establishmentsUpload.single('file'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: 'No file uploaded or invalid file type.' });
-  }
-  return res.status(200).json({ message: 'File uploaded successfully.', file: req.file });
-});
+const handleFileUpload = (upload: Multer) => (req: RequestProps, res: ResponseProps) => {
+  upload.single('file')(req, res, (error) => {
+    if (error) {
+      return res.status(400).json({ error: 'File upload failed.', details: (error as Error).message });
+    }
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded or invalid file type.' });
+    }
+    return res.status(200).json({ message: 'File uploaded successfully.', file: req.file });
+  });
+};
+
+uploadRouter.post('/establishments', handleFileUpload(establishmentsUpload));
+uploadRouter.post('/products', handleFileUpload(productsUpload));
+uploadRouter.post('/users', handleFileUpload(usersUpload));
 
 export default uploadRouter;
