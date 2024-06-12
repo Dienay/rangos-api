d275d4fda736a3ca0ecce785f048ce88be23c903
Dienay Lima <dienaylima@gmail.com>
2024-06-12 11:27:57 -0300

    feature: add user upload router

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UserController from '@/controllers/userController';
 import checkToken from '@/middlewares/checkToken';
+import { usersUpload } from '@/config/multer';
 
 // Create a new router instance
 export const userRouter = Router();
@@ -8,9 +9,9 @@ export const userRouter = Router();
 // Destructure the required methods from UserController
 const { signup, login, getUserById, updateUser, deleteUser } = UserController;
 // Define routes
-userRouter.post('/user/signup', signup);
-userRouter.post('/user/login', login);
+userRouter.post('/signup', usersUpload.single('avatar'), signup);
+userRouter.post('/login', login);
 // Define routes protected by the checkToken middleware
 userRouter.get('/user/:id', checkToken, getUserById);
-userRouter.put('/user/:id', checkToken, updateUser);
+userRouter.put('/user/:id', usersUpload.single('avatar'), checkToken, updateUser);
 userRouter.delete('/user/:id', checkToken, deleteUser);
