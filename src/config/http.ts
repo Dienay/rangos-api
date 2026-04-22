994bf6ca0a8f24ecdcabe8393cd4c4a4997b1e50
Dienay Lima <dienaylima@gmail.com>
2026-04-22 17:02:12 -0300

    refactor: remove custom express type aliases from config

--- src/config/http.ts
+++ src/config/http.ts
@@ -1,17 +1,12 @@
 // Import the required modules from 'express' and middleware libraries
-import express, { Router, Request, Response, NextFunction } from 'express';
+import express = require('express');
+import type { Express } from 'express';
 import helmet from 'helmet';
 import compression from 'compression';
 import cors from 'cors';
 
-// Define types for Router, Request, Response, and NextFunction
-type RouterProps = Router;
-type RequestProps = Request;
-type ResponseProps = Response;
-type NextFunctionProps = NextFunction;
-
 // Create an Express application instance
-const app = express();
+const app = express() as Express;
 
 // Apply middleware to enhance security, performance, and handling of JSON and URL-encoded data
 
@@ -30,4 +25,4 @@ app.use(express.json());
 // Parse incoming URL-encoded payloads
 app.use(express.urlencoded({ extended: true }));
 
-export { app, RouterProps, RequestProps, ResponseProps, NextFunctionProps };
+export { app };
